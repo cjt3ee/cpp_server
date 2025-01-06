14807182bf549f6dfdbb0b700960c67c87b68c1a
Jason Chen <i754057@AZRWEU029266.azure.global.corp.sap>
2025-01-06 07:16:04 +0000

    Initial commit in VDI.

---
+++ server.cpp
@@ -0,0 +1,9 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <fmt/format.h>
+
+int main()
+{
+    fmt::println("HAHA");
+}
---
+++ temp.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char ch[256] = {2};
+    cout << "res:" << (int)ch[5] << endl;
+}
