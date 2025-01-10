91fe343ba61564fdec05eed9b3cd2a6c1da16860
cjtree <chenjunshu@zju.edu.cn>
2025-01-10 09:08:49 +0000

    add header

--- server.cpp
+++ server.cpp
@@ -1,10 +1,15 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/epoll.h>
 #include <fmt/format.h>
 
 int main()
 {
-    fmt::print("HAHA\n");
+    fmt::println("HAHA\n");
     fmt::print("T\n");
 }
